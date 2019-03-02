b75ad14443e18748e10d1e8c7083aa5e30538902
sbradley36 <onetechsailor@gmail.com>
2019-03-02 13:32:40 -0800

    First commit

---
+++ ch02/Addition.java
@@ -0,0 +1,26 @@
+// Addition program
+import java.util.Scanner; 
+
+public class Addition
+{
+    public static void main(String[] args)
+    {
+        // Create a Scanner to obtain input. 
+        Scanner input = new Scanner(System.in);
+        // System.in is a standard input object. 
+
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();  // Read first number. 
+
+        System.out.print("Enter second integer: ");
+        number2 = input.nextInt();  
+
+        sum = number1 + number2; // assignment statement with binary operator
+
+        System.out.printf("Sum is %d%n", sum);  // %d means decimal integer. 
+    }
+}
---
+++ ch02/Welcome2.java
@@ -0,0 +1,10 @@
+public class Welcome2
+{
+    // Print a single line with two statements 
+    // using print vs. println class. 
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    }
+} // end class Welcome2
